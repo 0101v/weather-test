dd18392e1672515d345afcc9b82a30ec6df78a8a
0101v <375296956150@ya.ru>
2022-04-03 21:20:38 +0300

    feat: localStorage

--- src/store.js
+++ src/store.js
@@ -7,9 +7,17 @@ import rootSaga from './sagas'
 
 const sagaMiddleware = createSagaMiddleware()
 
+const persistedState = localStorage.getItem('reduxState') 
+                       ? JSON.parse(localStorage.getItem('reduxState'))
+                       : {}
+                       
 export const store = createStore(
   weatherReducer,
+  persistedState,
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 )
 
 sagaMiddleware.run(rootSaga)
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
