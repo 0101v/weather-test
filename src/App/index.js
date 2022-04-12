894050f14be43f3e2e62bddf8d54d19676a38e05
0101v <375296956150@ya.ru>
2022-04-12 22:51:18 +0300

    refactor

--- src/App/index.js
+++ src/App/index.js
@@ -7,11 +7,10 @@ import { getBackgroundImage } from "../helpers/getBackgroundImage";
 const App = () => {
   const { outside, timeZone } = useSelector(store => store)
 
-
   return (
     <>
       <GlobalStyles image={getBackgroundImage(outside, timeZone)}/>
-      <Container image={getBackgroundImage(outside, timeZone)}/>
+      <Container/>
     </>
   );
 }
